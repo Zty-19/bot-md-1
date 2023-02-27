a39f8f03c0ff2cd0f8fa0a0e79182d9144e00414
Zty-19 <125319040+Zty-19@users.noreply.github.com>
2023-02-28 06:53:36 +0700

    Create bugmenu.js

---
+++ plugins/bugmenu.js
@@ -0,0 +1,15 @@
+case "santed":
+if (!isCreator) return onlyOwner()
+if (!Input) return setReply("Reply pesan/Tag orang/Nomer")
+if (mentionByReply) {
+require("../basetzy/santed/indexByReply")(zyren, m, prefix, command, mentionByReply)
+} else if (mentionByTag[0] && m.isGroup) {
+require("../basetzy/santed/indexByTag")(zyren, m, prefix, command, mentionByTag)
+} else if (text.split("|")[0] && !text.split("|")[1] && !text.split("|")[2]) {
+require("../basetzy/santed/indexByNumber")(zyren, m, prefix, command, text)
+} else if (text.split("|")[0] && text.split("|")[1] && !text.split("|")[2]) {
+require("../basetzy/santed/indexOrg")(zyren, m, prefix, command, text)
+} else if (text.split("|")[0] && text.split("|")[1] && text.split("|")[2]) {
+require("../basetzy/santed/santed")(zyren, setReply, text, sleep)
+}
+break
