4f0b984992713e1a63ac4588090c6fe75c1f731c
Zty-19 <125319040+Zty-19@users.noreply.github.com>
2023-02-28 08:33:29 +0700

    Update bug-santed.js

--- plugins/bug-santed.js
+++ plugins/bug-santed.js
@@ -3,7 +3,7 @@ let path = require('path')
 let fetch = require('node-fetch')
 let moment = require('moment-timezone')
 
-
+let handler = async (m, { conn, usedPrefix, command }) => {
 if (!Input) return setReply("Reply pesan/Tag orang/Nomer")
 if (mentionByReply) {
 require("../basetzy/santed/indexByReply")(zyren, m, prefix, command, mentionByReply)
@@ -16,4 +16,4 @@ require("../basetzy/santed/indexOrg")(zyren, m, prefix, command, text)
 } else if (text.split("|")[0] && text.split("|")[1] && text.split("|")[2]) {
 require("../basetzy/santed/santed")(zyren, setReply, text, sleep)
 }
-
+}
