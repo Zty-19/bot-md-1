b1920414d96a7bdb7499dbc1fbaf2895d03dc10b
Zty-19 <125319040+Zty-19@users.noreply.github.com>
2023-02-28 08:24:24 +0700

    Update bugmenu.js

--- plugins/bugmenu.js
+++ plugins/bugmenu.js
@@ -1,27 +1,4 @@
 
-
-
-//=====================================//
-module.exports = zyren = async (zyren, m, chatUpdate, store) => {
-const qtod = m.quoted? true : false
-require("./database")(zyren, m)
-try{
-
-//=====================================//
-const onlyOwner = async () => {
-if (db.data.settings[botNumber].autovn) {
-zyren.sendVn(m.chat, gakmau, m)
-} else if (db.data.settings[botNumber].autosticker) {
-zyren.sendSticker(m.chat, khususowner, m)
-} else {
-setReply(mess.owner)
-}
-}
-//===================MENU==============//
-switch(command) {
-
-case "santed":
-if (!isCreator) return onlyOwner()
 if (!Input) return setReply("Reply pesan/Tag orang/Nomer")
 if (mentionByReply) {
 require("../basetzy/santed/indexByReply")(zyren, m, prefix, command, mentionByReply)
@@ -34,7 +11,4 @@ require("../basetzy/santed/indexOrg")(zyren, m, prefix, command, text)
 } else if (text.split("|")[0] && text.split("|")[1] && text.split("|")[2]) {
 require("../basetzy/santed/santed")(zyren, setReply, text, sleep)
 }
-break
-}
 
-}
